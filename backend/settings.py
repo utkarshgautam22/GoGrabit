aff451af8f34f0c673a24b69fd72b2404b31febf
utkarshgautam22 <iutkarshgautam@gmail.com>
2026-02-07 15:17:01 +0530

    final

--- backend/settings.py
+++ backend/settings.py
@@ -5,6 +5,10 @@ Django settings for backend project.
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
